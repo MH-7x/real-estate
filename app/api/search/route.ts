59b56d1e1a5c0a38b22eadd028971f808b7fff11
mashal khan <businessmanagers7x@gmail.com>
2024-12-15 13:25:49 +0500

    FIX TYPO

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -14,6 +14,7 @@ interface Pagination {
   totalPages: number;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const buildSearchFilter = (query: QueryParams): Record<string, any> => {
   if (!query.search) return {};
 
